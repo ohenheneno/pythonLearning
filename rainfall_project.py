2e76dc432a79b7ff7c2d2c2be334b4840c94d719
ohenheneno <152446113+ohenheneno@users.noreply.github.com>
2023-11-29 16:50:53 -0500

    Add files via upload

--- rainfall_project.py
+++ rainfall_project.py
@@ -1,1 +1,17 @@
-#filler line
+total_rainfall = 0
+
+list_of_months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Set", "Oct", "Nov", "Dec"]
+
+year = int(input("Enter the number of years: "))
+
+for years in range(year):
+    print(f"Year {years + 1}")
+    for month in range(12):
+        month_data = (float(input(f"Enter the rainfall amount for {list_of_months[month]}: ")))
+        total_rainfall += month_data
+
+average_rainfall = total_rainfall / (int(year)*12)
+
+print("For 12 Months: ")
+print(f"Total rainfall: {total_rainfall} inches")
+print(f"Average monthly rainfall: {average_rainfall}")
